492ce07b7708d35c2e567fc59f5f89454d2dea47
Mark Sagi-Kazar <mark.sagikazar@gmail.com>
2025-09-26 22:19:09 +0200

    build: update dev env
    Signed-off-by: Mark Sagi-Kazar <mark.sagikazar@gmail.com>

---
+++ devenv.nix
@@ -0,0 +1,20 @@
+{ pkgs, ... }:
+
+{
+  cachix.pull = [ "sagikazarmark-dev" ];
+
+  languages = {
+    go = {
+      enable = true;
+      package = pkgs.go_1_25;
+    };
+  };
+
+  packages = with pkgs; [
+    just
+    golangci-lint
+    xcaddy
+    dagger
+    semver-tool
+  ];
+}
